0e17f8afb2e2ae4a35e7eeaa4bedbfa6ff192f61
Dipannita_Mondal <dipannitamisti10@gmail.com>
2023-04-26 23:39:01 +0530

    heal button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 const ATTACK_VALUE = 10
+const STRONG_ATTACK_VALUE =17
 const MONSTER_ATTACK_VALUE = 14
+const HEAL_VALUE = 20
 
 let choseMaxLife = 100
 let currentMonsterhealth = choseMaxLife
@@ -7,14 +9,10 @@ let currentPlayerHealth = choseMaxLife
 
 adjustHealthBars(choseMaxLife)
 
-function attackHandler(){
-    const damage = dealMonsterDamage (ATTACK_VALUE)
-    currentMonsterhealth -= damage
+function endRound(){
     const playerDamage =dealPlayerDamage(MONSTER_ATTACK_VALUE)
     currentPlayerHealth -= playerDamage
-
-
-    if(currentMonsterhealth <= 0 && currentPlayerHealth > 0)
+   if(currentMonsterhealth <= 0 && currentPlayerHealth > 0)
     {
         alert("You won !!")
     }
@@ -26,7 +24,43 @@ function attackHandler(){
     {
         alert("You have a draw !!")
     }
+}
+
+function attackMonster(mode){
+    let maxDamage
+    if(mode === 'ATTACK'){
+        maxDamage = ATTACK_VALUE
+    }else if(mode === 'STRONG_ATTACK'){
+        maxDamage = STRONG_ATTACK_VALUE
+    }
+    const damage = dealMonsterDamage (maxDamage)
+    currentMonsterhealth -= damage
+    endRound();
+
+}
+
+function attackHandler(){
+   attackMonster('ATTACK')
+}
+
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK')
+}
+function healPlayerHandler(){
+
+    let healValue
+    if(currentPlayerHealth>=choseMaxLife - HEAL_VALUE){
+        alert("You can't heal to more than your max initial health")
+        healValue = choseMaxLife - currentPlayerHealth
+    }else{
+        healValue = HEAL_VALUE
+    }
+    increasePlayerHealth(healValue)
+    currentPlayerHealth += healValue
+    endRound()
 
 }
 
 attackBtn.addEventListener('click',attackHandler)
+strongAttackBtn.addEventListener('click',strongAttackHandler)
+healBtn('click',healPlayerHandler)
